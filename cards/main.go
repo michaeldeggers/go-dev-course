99a0afd6fa6144e24c837e3c4ac42ddf074c7e11
Michael Eggers <michaeldeggers@gmail.com>
2019-08-10 11:27:50 -0400

    Basic Changes
    - add demo_files dir and .gitkeep file within
    - modify code to store files from execution there
      - makes for easier global gitignore settings

--- cards/main.go
+++ cards/main.go
@@ -4,12 +4,13 @@ import "fmt"
 
 // understood that this both creates and reads the same file - for learning
 func main() {
+	filename := "demo_files/my_cards"
 	cards := newDeck()
 	cards.shuffleCards()
-	err := cards.saveToFile("my_cards")
+	err := cards.saveToFile(filename)
 	if err != nil {
 		fmt.Println("Error writing to file", err)
 	}
-	newCards := newDeckFromFile("my_cards")
+	newCards := newDeckFromFile(filename)
 	newCards.print()
 }
