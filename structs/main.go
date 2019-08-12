99432b44a291a2a1231169e7dad9ecdd3f507193
michaeldeggers <michaeldeggers@gmail.com>
2019-08-12 16:57:28 -0400

    - remove other definitions

--- structs/main.go
+++ structs/main.go
@@ -14,20 +14,6 @@ type contactInfo struct {
 }
 
 func main() {
-	// first way -- danger in not specifycing first / last fields
-	jack := person{"Jack", "Bro", contactInfo{"", 0}}
-	fmt.Printf("%+v", jack)
-
-	// second way -- better -- can reorder fields later without losing proper def
-	mike := person{firstName: "Mike", lastName: "Bro"}
-	fmt.Printf("%+v", mike)
-
-	// third way - initialize first, define later
-	var bob person
-	bob.firstName = "Bob"
-	bob.lastName = "Bro"
-	fmt.Printf("%+v", bob)
-
 	frank := person{
 		firstName: "Frank",
 		lastName:  "Bro",
@@ -36,5 +22,5 @@ func main() {
 			zipCode: 12345,
 		},
 	}
-	fmt.Println(frank)
+	fmt.Printf("%+v", frank)
 }
